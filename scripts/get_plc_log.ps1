3b7318254a1eaab1442b62914c8cd98b47237379
kawashiii <turquoise12-1@hotmail.co.jp>
2022-03-25 11:57:05 +0900

    update

--- scripts/get_plc_log.ps1
+++ scripts/get_plc_log.ps1
@@ -1,23 +1,26 @@
+$sleeping_time = 30
+Write-Host "Waiting for PLC log saved ($sleeping_time seconds)"
+Start-Sleep -Seconds $sleeping_time
+
 $path = Split-Path -Parent $MyInvocation.MyCommand.Path
 Set-Location $path
 
-$datetime = (get-date).ToString('yyyyMMdd_HHmmss')
+$datetime = (Get-Date).ToString('yyyyMMdd_HHmmss')
 $log_dir = 'log' + $datetime
-new-item -itemtype 'directory' -name $log_dir
+New-Item -ItemType 'directory' -Name $log_dir
 
-$plc_ip_address = '192.168.1.201'
+$plc_ip_address = '192.168.3.39'
 
-$get_dir_path_cmd =
-"open $plc_ip_address
-dfk
-dfk
-mls 2:\LOGGING\data mls_info.txt
-bye
-"
-$get_dir_name_cmd | Out-File get_dir_path_cmd.txt -Encoding UTF8
+$get_dir_path_cmd = "open $plc_ip_address`n"
+$get_dir_path_cmd += "dfk`n"
+$get_dir_path_cmd += "dfk`n"
+$get_dir_path_cmd += "mls 2:\LOGGING\data mls_info.txt`n"
+$get_dir_path_cmd += "bye`n"
+
+$get_dir_path_cmd | Out-File get_dir_path_cmd.txt -Encoding UTF8
 
 try {
-    ftp -i -s:get_folder_name_cmd.txt > $null
+    ftp -i -s:get_dir_path_cmd.txt > $null
     if (-not $?) {
         throw 'FTP failed'
     }
@@ -31,28 +34,27 @@ try {
     }
 
     foreach ($dir_path in $target_dir_path) {
-        $get_log_file_cmd = 
-        "open $plc_ip_address
-        dfk
-        dfk
-        mget $dir_path
-        bye
-        "
+        $get_log_file_cmd = "open $plc_ip_address`n"
+        $get_log_file_cmd += "dfk`n"
+        $get_log_file_cmd += "dfk`n"
+        $get_log_file_cmd += "mget $dir_path`n"
+        $get_log_file_cmd += "bye`n"
 
-        $get_log_file_cmd | Out-File get_log_file_cmd.txt -Encoding UTF-8
+        $get_log_file_cmd | Out-File get_log_file_cmd.txt -Encoding UTF8
 
         Set-Location $path/$log_dir
-        ftp -i -s:get_logfile_cmd.txt > $null
+        ftp -i -s:$path/get_log_file_cmd.txt > $null
 	if (-not $?) {
 	    throw 'FTP failed'
         }
 
         Set-Location $path
     }
+}
 catch {
     Set-Location $path
     $error_log_file = 'error_log' + $datetime + '.txt'
-    new-item -itemtype 'file' -name $error_log_file
+    New-Item -ItemType 'file' -Name $error_log_file
     Write-Output $_.Exception.Message | Add-Content $error_log_file
 }
 
